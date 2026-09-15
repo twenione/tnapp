6cae36738317f313d0e374524959eaafa57d0268
twenione <24428733+twenione@users.noreply.github.com>
2026-09-16 08:08:46 +0900

    build(phase2): centralize Kotlin plugin resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     base
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.2.20" apply false
 }
 
 group = "com.trailnav"
--- replay/build.gradle.kts
+++ replay/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
+    id("org.jetbrains.kotlin.jvm")
     application
 }
 
